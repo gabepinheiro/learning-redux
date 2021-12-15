1029d9131289f07f51ba87e71a62c3c32b70d2c9
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-15 20:54:52 -0300

    add product fetch in catalog component

--- src/components/catalog/index.tsx
+++ src/components/catalog/index.tsx
@@ -1,9 +1,33 @@
-import {useStore} from "react-redux"
+import {useState, useEffect} from 'react'
+import {api} from '../../services/api'
+import {Product} from '../../store/modules/cart'
+
 
 export const Catalog = () => {
-  const store = useStore()
+  const [products, setProducts] = useState<Product[]>([])
 
-  console.log(store.getState())
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await api.get('/products')
 
-  return <h1>Catalog</h1>
+      setProducts(response.data)
+    }
+
+    fetchProducts()
+  }, [])
+
+  return (
+    <main>
+      <h1>Catalog</h1>
+
+      {products.map(product => (
+        <article key={product.id}>
+          <strong>{product.title}</strong> {" - "}
+          <span>R$ {product.price}</span> {" "}
+
+          <button>Comprar</button>
+        </article>
+      ))}
+    </main>
+  )
 }
