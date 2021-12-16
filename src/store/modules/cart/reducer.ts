e1e3791d49dff39441e7c62d2e557fce8afa894c
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-16 00:38:10 -0300

    add switch case and case 'ADD_PRODUCT_TO_CART' on cart reducer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,8 +5,24 @@ const INITIAL_VALUE: Cart = {
   items: []
 }
 
-export const cart: Reducer<Cart> = (state, action) => {
-  console.log(state, action)
+export const cart: Reducer<Cart> = (state = INITIAL_VALUE, action) => {
+  switch (action.type) {
+    case 'ADD_PRODUCT_TO_CART':
+      const {product} = action.payload
 
-  return INITIAL_VALUE
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+            product,
+            quantity: 1
+          }
+        ]
+      }
+
+    default:
+      return state
+
+  }
 }
