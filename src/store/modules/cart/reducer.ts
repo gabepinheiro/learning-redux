832fe141bc06b66bb280b20f6406dac5d0b8c7d6
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-16 01:19:51 -0300

    add immer and produce on cart reducer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,28 +1,27 @@
 import {Reducer} from "redux";
 import {Cart} from "./types";
 
+import {produce} from 'immer'
+
 const INITIAL_VALUE: Cart = {
   items: []
 }
 
 export const cart: Reducer<Cart> = (state = INITIAL_VALUE, action) => {
-  switch (action.type) {
-    case 'ADD_PRODUCT_TO_CART':
-      const {product} = action.payload
+  return produce(state, draft => {
+    switch (action.type) {
+      case 'ADD_PRODUCT_TO_CART':
+        const {product} = action.payload
 
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1
-          }
-        ]
-      }
+        draft.items.push({
+          product,
+          quantity: 1
+        })
 
-    default:
-      return state
+        break;
+      default:
+        return state
 
-  }
+    }
+  })
 }
