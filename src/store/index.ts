6f77861cf41a02e8685cfc3ff69a5f48af92cdbe
Gabe Pinheiro <gabrielpsilvas@live.com>
2021-12-16 13:27:13 -0300

    add redux-devtools

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,8 +3,13 @@ import {Cart} from './modules/cart'
 
 import {rootReducer} from './rootReducer'
 
+import {composeWithDevTools} from 'redux-devtools-extension'
+
 export type State = {
   cart: Cart
 }
 
-export const store = createStore(rootReducer)
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+)
